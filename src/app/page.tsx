d7ac5f96793eba6ff768e5c1daba7b160d3b5489
Sax Dhiut <mail@iyxeyl.me>
2025-04-14 22:11:04 +0400

    feat: add fullscreen toggle button to main page for enhanced user experience

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,10 @@
 "use client"
 
+import { Button } from "@/components/ui/button"
 import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/label"
 import { cn } from "@/lib/utils"
+import { viewport } from "@telegram-apps/sdk-react"
 
 export default function Page() {
 	// Array of 20 numbers from 0 to 19
@@ -10,6 +12,20 @@ export default function Page() {
 
 	return (
 		<div className={cn("space-y-6")}>
+			<Button
+				onClick={() => {
+					if (viewport.requestFullscreen.isAvailable()) {
+						if (viewport.isFullscreen()) {
+							viewport.exitFullscreen()
+							return
+						}
+
+						viewport.requestFullscreen()
+					}
+				}}
+			>
+				Toggle Fullscreen
+			</Button>
 			{numbers.map((number) => (
 				<div key={number} className={cn("space-y-2")}>
 					<Label>Input {number}</Label>
